0ba3b49e65d5df1c4d460cdc0b287550d8edd1c4
ParkerKaiMalachai <irinkademchuuuuuuuk@gmail.com>
2023-02-14 22:17:34 +0300

    index (hw-2) homework

---
+++ index2.js
@@ -0,0 +1,53 @@
+let x=20;
+let y=58;
+let z=42;
+let a=x +y + z;
+console.log (a);
+let minute=60;
+let hour=60;
+let day=24;
+let year=365;
+let myage=22;
+let mydays=myage * year;
+let myhours=mydays * day;
+let myminutes=myhours * hour;
+let myseconds=myminutes * minute;
+alert(`My age in seconds is ${myseconds}`);
+let count = 42;
+let userName = '42';
+let stringcount = String(count);
+console.log (typeof stringcount);
+let numberuserName = Number(userName);
+console.log(typeof numberuserName);
+let f = 1;
+let b = 2;
+let c = " белых медведей";
+let fstring = String(f);
+let bstring = String(b);
+let sum = fstring + bstring + c;
+console.log (sum)
+let access = 'доступ';
+let soldier = 'морпех';
+let naled = 'наледь';
+let poprek = 'попрек';
+let rubilo = 'рубило';
+var lengthAccess = access.length;
+console.log (lengthAccess)
+var lengthSoldier = soldier.length;
+console.log (lengthSoldier)
+var lengthNaled = naled.length;
+console.log (lengthNaled)
+var lengthPoprek = poprek.length;
+console.log (lengthPoprek)
+var lengthRubilo = rubilo.length;
+console.log (lengthRubilo)
+var lengthWords = lengthAccess + lengthNaled + lengthPoprek + lengthSoldier + lengthRubilo;
+console.log (lengthWords)
+let firsttype = 43;
+let secondtype = 'hello ilyha';
+let thirdtype = true;
+console.log ('Variable: firsttype have type:' +typeof firsttype)
+let UserName = prompt('Ввелите ваше имя');
+let UserAge = prompt('Введите ваш возраст');
+console.log ('Ваше имя ' +UserName, 'Ваш возраст ' +UserAge)
+
