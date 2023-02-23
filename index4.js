bcb0899c3345bbb0b87da4aeb34b106cb4ae203c
ParkerKaiMalachai <irinkademchuuuuuuuk@gmail.com>
2023-02-23 19:04:10 +0300

    IRINA hw-4 task 5!

--- index4.js
+++ index4.js
@@ -32,6 +32,22 @@ function getSumNumbers (numberPar) {
     console.log (sum)
 }
 getSumNumbers (2023)
+//5
+function getSumAgain(a, b) {
+    let sum = 0;
+    if (a < b){
+        for (let i = a; i <= b; i++){
+            sum+=i;
+        }
+        console.log (sum)
+    } else if (a > b){
+        for (i = b; i <= a; i++){
+            sum+=i;
+        }
+        console.log (sum)
+    }
+}
+getSumAgain (8,5)
 //6
 function main (booleanPar, firstFun, secondFun) {
     if (booleanPar) {
