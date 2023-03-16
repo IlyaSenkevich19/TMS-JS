0e3a49e03ddd3fc00432e983c913b72505afd5a3
DennisVatanabe <4dennis4parmon@gmail.com>
2023-03-16 19:32:28 +0300

    lesson_10

--- app.js
+++ app.js
@@ -18,3 +18,36 @@ a.style.justifyContent = 'center';
 a.style.backgroundColor = 'blue';
 a.style.color = 'orange';
 a.style.margin = '0 50px';
+
+const button1 = document.querySelector('#button1');
+const button2 = document.querySelector('#button2');
+const button3 = document.querySelector('#button3');
+
+const one = document.querySelector('#one');
+const two = document.querySelector('#two');
+const three = document.querySelector('#three');
+
+
+button1.addEventListener('click', (e) => {
+    one.classList.add('four');
+    two.classList.remove('four')
+    three.classList.remove('four')
+    console.log(1);
+    setTimeout(() => one.classList.remove('four'), 5000)
+});
+
+button2.addEventListener('click', (e) => {
+    two.classList.add('four')
+    one.classList.remove('four')
+    three.classList.remove('four')
+    setTimeout(() => two.classList.remove('four'), 5000)
+    console.log(2);
+});
+
+button3.addEventListener('click', (e) => {
+    three.classList.add('four');
+    two.classList.remove('four')
+    one.classList.remove('four')
+    console.log(3);
+    setTimeout(() => three.classList.remove('four'), 5000)
+});
