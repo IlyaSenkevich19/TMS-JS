4b0f09c23383bd92b7940eb03a331289cb82fd70
PavelBaranouski <js.baranouski@gmail.com>
2023-03-22 21:38:21 +0300

    part1

---
+++ hw_11/index.js
@@ -0,0 +1,35 @@
+//date
+let nowDate = new Date().toLocaleDateString()
+
+//specialId
+function specialId(length){
+    let uniqueId = "";
+    const idSymbols = "QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm123456789";
+    const idymbolsLength = idSymbols.length;
+    for(let i = 0; i < length; i++){
+        uniqueId += idSymbols.charAt(Math.floor(Math.random() * idymbolsLength))
+    }
+    return uniqueId
+}
+const randomId = specialId(10)
+
+//
+const todos = [];
+//
+const todo = {
+    id: randomId,
+    Date: nowDate ,
+    text:'Play video games',
+    isChecked: true,
+}
+console.log(todo)
+//
+function setName(){
+    let obj = 
+}
+
+
+
+
+
+  
